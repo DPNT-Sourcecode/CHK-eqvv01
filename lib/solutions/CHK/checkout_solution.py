a54d6aefec13da2aebc7ac747c5d2d34ece617aa
sbx_user1051 <sbx_user1051@169.254.12.89>
2024-01-16 15:22:00 +0000

    Tue Jan 16 15:22:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,10 +21,14 @@ def count_items(skus, prices):
         else:
             quantities[item] = 0
 
+
 def calculate_value(item_count, prices, special_offers):
     value = 0
     for key in item_count:
         if key in special_offers:
-            if special_offers[key][0] <= item_count[key]:
+            while special_offers[key][0] <= item_count[key]:
                 value += special_offers[key][1]
                 item_count[key] -= special_offers[key][0]
+        value += (item_count[key] * prices[key])
+
+    return value
