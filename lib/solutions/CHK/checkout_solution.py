c2ddbb75841215c14012431434bcbef72f617473
sbx_user1051 <sbx_user1051@169.254.12.89>
2024-01-16 15:58:30 +0000

    Tue Jan 16 15:58:30 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,14 +4,15 @@
 # skus = unicode string
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    special_offers = {'A': [3, 130], 'B': [2, 45]}
+    special_offers = {'A': [[5, 200], [3, 130]], 'B': [[2, 45]]}
+    itemsfree_offers = {'E': {'B': 1}}
     item_count = count_items(skus, prices)
 
     if item_count == -1:
         # invalid input
         return -1
 
-    checkout_value = calculate_value(item_count, prices, special_offers)
+    checkout_value = calculate_value(item_count, prices, special_offers, itemsfree_offers)
     return checkout_value
 
 
@@ -32,7 +33,16 @@ def count_items(skus, prices):
     return quantities
 
 
-def calculate_value(item_count, prices, special_offers):
+def calculate_value(item_count, prices, special_offers, itemsfree_offers):
+    print(item_count)
+    # check if there is an item free offer, if so update the item count accordingly
+    for itemkey in item_count:
+        if itemkey in itemsfree_offers:
+            for offerkey, value in itemsfree_offers[itemkey].items():
+                item_count[offerkey] -= value
+
+    print(item_count)
+
     value = 0
     for key in item_count:
         if key in special_offers:
