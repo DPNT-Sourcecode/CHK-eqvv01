207c9e7579ad436632c65256c55c3cb268a9593e
sbx_user1051 <sbx_user1051@169.254.22.101>
2024-01-16 15:04:02 +0000

    Tue Jan 16 15:04:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,11 +5,23 @@
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     special_offers = {'A': '3 130', 'B': '2 45'}
-    quantities = {}
+    item_count = count_items(skus, prices)
 
+
+def count_items(skus, prices):
+    quantities = {}
     for item in skus:
         if item not in prices:
+            # invalid input
             return -1
 
-        quantities[item] += 1
+        # update item count
+        if item in quantities:
+            quantities[item] += 1
+        else:
+            quantities[item] = 0
+
+def calculate_value(item_count, prices, special_offers):
+    value = 0
+    
 
