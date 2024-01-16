e878a7c98a792c2c2d0a89514dbb69befb07db54
sbx_user1051 <sbx_user1051@169.254.22.101>
2024-01-16 14:58:02 +0000

    Tue Jan 16 14:58:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return skus
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    special_offers = {'A': '3 130', 'B': '2 45'}
+    quantities = {}
 
+    for item in skus:
+        if item not in prices:
+            return -1
+
+        quantities[item] += 1
 
